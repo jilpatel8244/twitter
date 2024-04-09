afee9f2262f8feac99ae02737d9a19b94a7597c7
parmeshvar.baria <parmeshvar.baria@esparkbizmail.com>
2024-04-09 19:24:49 +0530

    added login.ejs

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
-var connection = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "password",
-    database : process.env.DB_NAME || 'temp_twitter'
-}).promise();
+var connection = mysql
+  .createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PASS || "password",
+    database: process.env.DB_NAME || "temp_twitter",
+  })
+  .promise();
 
 module.exports = connection;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const logger = require('../../logger/logger');
+const express = require("express");
+const logger = require("../../logger/logger");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    logger.info("information");
-    logger.debug("debug");
-    logger.warn("warning");
-    res.render('pages/demo');
-})
+  logger.info("information");
+  res.render("pages/demo");
+});
 
+router.get("/login", (req, res) => {
+  logger.info("in login page");
+  res.render("pages/login");
+});
 module.exports = router;
