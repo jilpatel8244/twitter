29861e99ab84f3ec7980b4a448c157437b0ab431
mihir-rajpopat <mihir.rajpopat.2024@gmail.com>
2024-04-11 17:57:04 +0530

    work on explore module do some analysys and make up in explore.ejs file and also do api in explore route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,43 +8,51 @@ const logger = require("../../logger/logger");
 // const { verify_user_byemail } = require('../controller/verify_user_byemail');
 // const { get_registration,get_password } = require('../controller/registration');
 // const { resetpassword } = require('../controller/resetpassword');
-const {post_registration,get_registration} = require('../controller/registration');
-const {getPassword,postPassword} = require('../controller/password');
-const passport = require('passport');
-require('../middleware/passport');
+// const { post_registration, get_registration } = require('../controller/registration');
+// const { getPassword, postPassword } = require('../controller/password');
+// const passport = require('passport');
+// require('../middleware/passport');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    logger.info("information");
-    logger.debug("debug");
-    logger.warn("warning");
-    res.render('pages/demo');
-})
-const { forgotpassword } = require('../controller/auth.controller');
-const { verify_user_byemail } = require('../controller/verify_user_byemail');
-const { get_registration,get_password } = require('../controller/registration');
 
-const { resetpassword } = require('../controller/resetpassword');
-const { post_registration } = require("../controller/registration");
-
-const {post_registration} = require('../controller/registration');
+const exploreroute = require("../routes/explore.routes")
 
 
-router.get("/", (req, res) => {
-  logger.info("information");
-  res.render("pages/index");
-});
 
-router.get("/registration", get_registration);
-router.get("/login", (req, res) => {
-  logger.info("in login page");
-  res.render("pages/login");
-});
+//change by mihir date : 11 - 04 
+router.use("/explore", exploreroute)
 
-router.post("/registration", post_registration);
+// router.get("/", (req, res) => {
+//   logger.info("information");
+//   logger.debug("debug");
+//   logger.warn("warning");
+//   res.render('pages/demo');
+// })
+// const { forgotpassword } = require('../controller/auth.controller');
+// const { verify_user_byemail } = require('../controller/verify_user_byemail');
+// const { get_registration, get_password } = require('../controller/registration');
 
-router.get('/password', getPassword);
-router.post('/password', postPassword);
+// const { resetpassword } = require('../controller/resetpassword');
+// const { post_registration } = require("../controller/registration");
+
+// const { post_registration } = require('../controller/registration');
+
+
+// router.get("/", (req, res) => {
+//   logger.info("information");
+//   res.render("pages/index");
+// });
+
+// router.get("/registration", get_registration);
+// router.get("/login", (req, res) => {
+//   logger.info("in login page");
+//   res.render("pages/login");
+// });
+
+// router.post("/registration", post_registration);
+
+// router.get('/password', getPassword);
+// router.post('/password', postPassword);
 
 
 
@@ -62,25 +70,25 @@ router.post('/password', postPassword);
 // });
 
 
-router.get("/login", login);
+// router.get("/login", login);
 
-router.post('/login', loginHandler);
+// router.post('/login', loginHandler);
 
-router.get('/home', passport.authenticate('jwt', {session: false } ), (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "on home page"
-    })
-});
+// router.get('/home', passport.authenticate('jwt', { session: false }), (req, res) => {
+//   res.status(200).json({
+//     success: true,
+//     message: "on home page"
+//   })
+// });
 
-//roter for the forgot password
+// //roter for the forgot password
 
-router.get("/forgotpassword", forgotpassword)
-router.post("/verify_email", verify_user_byemail)
-router.post("/resetpasswordl", resetpassword)
+// router.get("/forgotpassword", forgotpassword)
+// router.post("/verify_email", verify_user_byemail)
+// router.post("/resetpasswordl", resetpassword)
 
 
-router.get("/registration", get_registration)
-router.get("/password", get_password)
+// router.get("/registration", get_registration)
+// router.get("/password", get_password)
 
 module.exports = router;
---
+++ src/routes/explore.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const logger = require("../../logger/logger");
+const { getExplorePage } = require("../controller/exploreControler/getexplore");
+
+const router = express.Router();
+
+
+
+router.get("/", getExplorePage)
+
+router.get("/" , )
+
+module.exports = router;
