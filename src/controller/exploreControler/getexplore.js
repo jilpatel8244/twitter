e2354601929d0561eea4c48212e246bb32eb1f27
mihir-rajpopat <mihir.rajpopat.2024@gmail.com>
2024-04-15 13:26:34 +0530

    api for get hastag or usrname based onchange event

--- src/controller/exploreControler/getexplore.js
+++ src/controller/exploreControler/getexplore.js
@@ -2,20 +2,35 @@ const connection = require("../../../config/connection");
 const ShortUniqueId = require("short-unique-id");
 
 exports.getExplorePage = async (req, res) => {
-  res.render("pages/explore");
+    res.render("pages/explore");
 };
 
-exports.getActivecode = async (req, res) => {
-  const uid = new ShortUniqueId({ length: 12 });
-  console.log("hello");
-  let Activationcode = uid.rnd();
-  let email = req.body.email;
-  try {
-    let sql = `UPDATE users SET activation_code ='${Activationcode}'  WHERE email = '${email}'`;
-    let [result] = await connection.query(sql);
-  } catch (error) {
-    console.log(error);
-  }
 
-  res.json({ code: Activationcode });
+// api for the get all usename and hatag based in search box on change event 
+exports.getUsernameOrHastagOnchage = async (req, res) => {
+
+    let searchbox = "mihir"
+    if (!searchbox) {
+        return res.json({ msg: "not found" })
+    }
+    if (searchbox.charAt(0) == "#") {
+        try {
+            let sql = `SELECT DISTINCT hashtag_name FROM hashtag_lists WHERE hashtag_name LIKE '${searchbox.substring(1)}%'`
+
+            let [result] = connection.query(sql);
+            console.log(result);
+
+        } catch (error) {
+            return res.json({ error: error })
+        }
+    }
+    else {
+        try {
+            let sql = `SELECT * FROM users WHERE username LIKE '${searchbox}%'`
+            let [result] = connection.query(sql);
+            console.log(result);
+        } catch (error) {
+            return res.json({ error: error })
+        }
+    }
 };
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -2,8 +2,8 @@ const logger = require("../../logger/logger");
 const express = require('express');
 const router = express.Router();
 
-const {tweetCreate,insertTweet} = require('../controller/tweet.controller')
+const { tweetCreate, insertTweet } = require('../controller/tweet.controller')
 
-router.get('/',tweetCreate);
-router.post('/insertTweet',insertTweet)
+router.get('/', tweetCreate);
+router.post('/insertTweet', insertTweet)
 module.exports = router
