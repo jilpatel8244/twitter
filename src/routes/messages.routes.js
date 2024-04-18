e94c17d5c3e8c40c87fe3e8e369568541c7db587
jilpatel8244 <jil.patel.2024@gmail.com>
2024-04-18 18:31:30 +0530

    improve design of message section and implement functionality of image send using multer in message

--- src/routes/messages.routes.js
+++ src/routes/messages.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { getMessagesPage, storeMessageHandler } = require("../controller/messages.controller");
+const { upload } = require("../middleware/multer");
 const router = express.Router();
 
 router.get("/", getMessagesPage);
-router.post("/storeMessage", storeMessageHandler);
+router.post("/storeMessage", upload.single('imgFile'), storeMessageHandler);
 
 module.exports = router;
