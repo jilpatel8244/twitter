1dda7d1e142ba5cfde3f6cb5634b6ca33daef7b1
mihir-rajpopat <mihir.rajpopat.2024@gmail.com>
2024-04-19 12:08:15 +0530

    complate all api which is used in explore section all are work completly and all are tested

--- src/controller/exploreControler/getexplore.js
+++ src/controller/exploreControler/getexplore.js
@@ -1,4 +1,4 @@
-const connection1 = require("../../../config/connection");
+const connection = require("../../../config/connection");
 const ShortUniqueId = require("short-unique-id");
 
 exports.getExplorePage = async (req, res) => {
@@ -8,7 +8,11 @@ exports.getExplorePage = async (req, res) => {
 // api for the get all usename and hatag based in search box on change event 
 exports.getUsernameOrHastagOnchage = async (req, res) => {
 
-    let searchbox = "mihir"
+    let searchbox = "#mihir"
+
+
+    searchbox = searchbox.trim();
+
     if (!searchbox) {
         return res.json({ msg: "not found" })
     }
@@ -16,8 +20,8 @@ exports.getUsernameOrHastagOnchage = async (req, res) => {
         try {
             let sql = `SELECT DISTINCT hashtag_name FROM hashtag_lists WHERE hashtag_name LIKE '${searchbox.substring(1)}%'`
 
-            let [result] = connection.query(sql);
-            console.log(result);
+            let [result] = await connection.query(sql);
+            return res.json({ result: result })
 
         } catch (error) {
             return res.json({ error: error })
@@ -25,60 +29,93 @@ exports.getUsernameOrHastagOnchage = async (req, res) => {
     }
     else {
         try {
-            let sql = `SELECT * FROM users WHERE username LIKE '${searchbox}%'`
-            let [result] = connection.query(sql);
-            console.log(result);
+            let sql = `SELECT * FROM users WHERE username LIKE '${searchbox}%' and is_active=1`
+            let [result] = await connection.query(sql);
+            return res.json({ result: result })
+
+
         } catch (error) {
             return res.json({ error: error })
         }
     }
 };
 
+
 exports.getTopTweetAndHastag = async (req, res) => {
 
+    try {
 
-    console.log("body is ", req.body);
-    let TopTweet = `        
-    SELECT users.username,
-    users.id as user_id, 
-    users.name, 
-    COALESCE(users.profile_img_url, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOFx557XPIXXmnhk7joe2Pq2uQhb1iCJ688RgQZzH5ZA&s') as profile_img_url, 
-    tweets.content,
-    tweets.id as tweet_id,
-    tweet_comments.content as comments, 
-    CASE
-      WHEN tweets.updated_at IS NOT NULL THEN
-        CASE
-          WHEN TIMESTAMPDIFF(SECOND, tweets.updated_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(SECOND, tweets.updated_at, NOW()+1), ' seconds ago')
-          WHEN TIMESTAMPDIFF(MINUTE, tweets.updated_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(MINUTE, tweets.updated_at, NOW()), ' minutes ago')
-          WHEN TIMESTAMPDIFF(HOUR, tweets.updated_at, NOW()) < 24 THEN CONCAT(TIMESTAMPDIFF(HOUR, tweets.updated_at, NOW()), ' hours ago')
-          ELSE CONCAT(DATE_FORMAT(tweets.updated_at, '%d'), ' ', DATE_FORMAT(tweets.updated_at, '%M'))
-        END
-      ELSE
-        CASE
-          WHEN TIMESTAMPDIFF(SECOND, tweets.created_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(SECOND, tweets.created_at, NOW()+1), ' seconds ago')
-          WHEN TIMESTAMPDIFF(MINUTE, tweets.created_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(MINUTE, tweets.created_at, NOW()), ' minutes ago')
-          WHEN TIMESTAMPDIFF(HOUR, tweets.created_at, NOW()) < 24 THEN CONCAT(TIMESTAMPDIFF(HOUR, tweets.created_at, NOW()), ' hours ago')
-          ELSE CONCAT(DATE_FORMAT(tweets.created_at, '%d'), ' ', DATE_FORMAT(tweets.created_at, '%M'))
-        END
-    END as time,
-    COALESCE(medias.media_url) as media_url
-  FROM users
-  JOIN tweets ON users.id = tweets.user_id
-  LEFT JOIN medias ON tweets.id = medias.tweet_id
-  LEFT JOIN tweet_comments ON tweet_comments.user_id = tweets.id 
-  WHERE users.is_active = 1 AND tweets.is_posted = 1
-  ORDER BY 
-    CASE
-      WHEN tweets.updated_at IS NOT NULL THEN tweets.updated_at
-      ELSE tweets.created_at
-    END DESC
+        let TopTweet = `
+        SELECT users.username,
+          users.id as user_id, 
+          users.name, 
+          users.profile_img_url as profile_img_url, 
+          tweets.content,
+          tweets.id as tweet_id,
+          tweet_comments.content as comments, 
+          CASE
+            WHEN tweets.updated_at IS NOT NULL THEN
+              CASE
+                WHEN TIMESTAMPDIFF(SECOND, tweets.updated_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(SECOND, tweets.updated_at, NOW()+1), ' seconds ago')
+                WHEN TIMESTAMPDIFF(MINUTE, tweets.updated_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(MINUTE, tweets.updated_at, NOW()), ' minutes ago')
+                WHEN TIMESTAMPDIFF(HOUR, tweets.updated_at, NOW()) < 24 THEN CONCAT(TIMESTAMPDIFF(HOUR, tweets.updated_at, NOW()), ' hours ago')
+                ELSE CONCAT(DATE_FORMAT(tweets.updated_at, '%d'), ' ', DATE_FORMAT(tweets.updated_at, '%M'))
+              END
+            ELSE
+              CASE
+                WHEN TIMESTAMPDIFF(SECOND, tweets.created_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(SECOND, tweets.created_at, NOW()+1), ' seconds ago')
+                WHEN TIMESTAMPDIFF(MINUTE, tweets.created_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(MINUTE, tweets.created_at, NOW()), ' minutes ago')
+                WHEN TIMESTAMPDIFF(HOUR, tweets.created_at, NOW()) < 24 THEN CONCAT(TIMESTAMPDIFF(HOUR, tweets.created_at, NOW()), ' hours ago')
+                ELSE CONCAT(DATE_FORMAT(tweets.created_at, '%d'), ' ', DATE_FORMAT(tweets.created_at, '%M'))
+              END
+          END as time,
+          medias.media_url as media_url
+          FROM users
+          JOIN tweets ON users.id = tweets.user_id
+          LEFT JOIN medias ON tweets.id = medias.tweet_id
+          LEFT JOIN tweet_comments ON tweet_comments.user_id = tweets.id 
+          WHERE tweets.content LIKE '%demo%' OR tweets.id IN (
+                  SELECT tweet_id FROM hashtag_tweet WHERE hashtag_id IN (
+                      SELECT id FROM hashtag_lists WHERE hashtag_name LIKE '%demo%'
+                  )
+              ) OR tweets.user_id IN (
+                  SELECT id FROM users WHERE username LIKE '%demo%'
+              )
+              ORDER BY (
+                  SELECT COUNT(*) FROM tweet_likes WHERE tweet_likes.tweet_id = tweets.id AND tweet_likes.status = 1
+              ) DESC
+        ;
       `
-    let [result1] = await connection1.query(TopTweet);
-    console.log(result1);
-    res.json({ resultTweet: result1 })
-}
+        let [result1] = await connection.query(TopTweet);
 
+        return res.json({ resultTweet: result1 })
+
+    } catch (error) {
+
+        console.log(error);
+        return res.send(error)
+    }
+};
+
+exports.getHastag = async (req, res) => {
+
+    try {
+        let TopHashtag = `SELECT hl.hashtag_name, COUNT(ht.hashtag_id) AS usage_count
+        FROM hashtag_lists hl
+        JOIN hashtag_tweet ht ON hl.id = ht.hashtag_id
+        GROUP BY hl.hashtag_name
+        ORDER BY usage_count DESC;`
+
+        let [result] = await connection.query(TopHashtag);
+
+        res.json({ resultHastag: result })
+
+
+    } catch (error) {
+        res.json({ error })
+
+    }
+}
 
 exports.getUsername = async (req, res) => {
 
@@ -88,9 +125,11 @@ exports.getUsername = async (req, res) => {
     }
 
     try {
-        let sql = `SELECT * FROM users WHERE username LIKE '${searchbox}%'`
-        let [result] = connection.query(sql);
-        console.log(result);
+        let sql = `      
+      
+        SELECT * FROM users WHERE username LIKE '%demo%' and is_active = 1;`
+        let [result] = await connection.query(sql);
+        res.json({ username: result })
     } catch (error) {
         return res.json({ error: error })
     }
@@ -99,22 +138,25 @@ exports.getUsername = async (req, res) => {
 
 exports.getMedia = async (req, res) => {
 
-    let searchbox = "mihir"
+    let searchbox = req.body.searchbox;
+
     if (!searchbox) {
         return res.json({ msg: "not found" })
     }
 
     try {
+        searchbox = searchbox.trim()
         let sql = `SELECT   distinct medias.media_url
         FROM medias
         JOIN tweets ON medias.tweet_id = tweets.id
         LEFT JOIN hashtag_tweet ON tweets.id = hashtag_tweet.tweet_id
         LEFT JOIN hashtag_lists ON hashtag_tweet.hashtag_id = hashtag_lists.id
         JOIN users ON tweets.user_id = users.id
-        WHERE (users.username LIKE '%demo%' OR tweets.content LIKE '%mihir%' OR hashtag_lists.hashtag_name LIKE '%demo%')
-        AND medias.media_type = 'photo';`
-        let [result] = connection.query(sql);
-        console.log(result);
+        WHERE (users.username LIKE '%${searchbox}%' OR tweets.content LIKE '%${searchbox}%' OR hashtag_lists.hashtag_name LIKE '%${searchbox}%')
+        AND medias.media_type LIKE '%image%';`
+        let [result] = await connection.query(sql);
+        res.json({ media: result })
+
     } catch (error) {
         return res.json({ error: error })
     }
@@ -130,15 +172,43 @@ exports.getLatestTweet = async (req, res) => {
     }
     if (searchbox.charAt(0) == "#") {
         try {
-            let sql = `SELECT tweets.*
-        FROM tweets
-        JOIN hashtag_tweet ON tweets.id = hashtag_tweet.tweet_id
-        JOIN hashtag_lists ON hashtag_tweet.hashtag_id = hashtag_lists.id
-        WHERE hashtag_lists.hashtag_name = '${searchbox.substring(1)}'
-        ORDER BY tweets.created_at DESC;`
+            let sql = `
+            SELECT users.username,
+              users.id as user_id, 
+              users.name, 
+              users.profile_img_url as profile_img_url, 
+              tweets.content,
+              tweets.id as tweet_id,
+              tweet_comments.content as comments, 
+              CASE
+                WHEN tweets.updated_at IS NOT NULL THEN
+                  CASE
+                    WHEN TIMESTAMPDIFF(SECOND, tweets.updated_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(SECOND, tweets.updated_at, NOW()+1), ' seconds ago')
+                    WHEN TIMESTAMPDIFF(MINUTE, tweets.updated_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(MINUTE, tweets.updated_at, NOW()), ' minutes ago')
+                    WHEN TIMESTAMPDIFF(HOUR, tweets.updated_at, NOW()) < 24 THEN CONCAT(TIMESTAMPDIFF(HOUR, tweets.updated_at, NOW()), ' hours ago')
+                    ELSE CONCAT(DATE_FORMAT(tweets.updated_at, '%d'), ' ', DATE_FORMAT(tweets.updated_at, '%M'))
+                  END
+                ELSE
+                  CASE
+                    WHEN TIMESTAMPDIFF(SECOND, tweets.created_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(SECOND, tweets.created_at, NOW()+1), ' seconds ago')
+                    WHEN TIMESTAMPDIFF(MINUTE, tweets.created_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(MINUTE, tweets.created_at, NOW()), ' minutes ago')
+                    WHEN TIMESTAMPDIFF(HOUR, tweets.created_at, NOW()) < 24 THEN CONCAT(TIMESTAMPDIFF(HOUR, tweets.created_at, NOW()), ' hours ago')
+                    ELSE CONCAT(DATE_FORMAT(tweets.created_at, '%d'), ' ', DATE_FORMAT(tweets.created_at, '%M'))
+                  END
+              END as time,
+              medias.media_url as media_url
+              FROM users
+              JOIN tweets ON users.id = tweets.user_id
+              LEFT JOIN medias ON tweets.id = medias.tweet_id
+              LEFT JOIN tweet_comments ON tweet_comments.user_id = tweets.id 
+              JOIN hashtag_tweet ON tweets.id = hashtag_tweet.tweet_id 
+              JOIN hashtag_lists ON hashtag_tweet.hashtag_id = hashtag_lists.id
+              WHERE  hashtag_lists.hashtag_name like '%${searchbox.substring(1)}%'
+              ORDER BY tweets.created_at DESC
+              ;`
 
-            let [result] = connection.query(sql);
-            console.log(result);
+            let [result] = await connection.query(sql);
+            return res.json({ resultTweet: result })
 
         } catch (error) {
             return res.json({ error: error })
@@ -146,13 +216,40 @@ exports.getLatestTweet = async (req, res) => {
     }
     else {
         try {
-            let sql = `SELECT *
-        FROM tweets
-        JOIN users ON tweets.user_id = users.id
-        WHERE tweets.content LIKE '%demo%' OR users.username LIKE '%demo%' OR users.name LIKE '%demo%'
-        ORDER BY tweets.created_at DESC`
-            let [result] = connection.query(sql);
-            console.log(result);
+            let sql = `SELECT users.username,
+            users.id as user_id, 
+            users.name, 
+            users.profile_img_url as profile_img_url, 
+            tweets.content,
+            tweets.id as tweet_id,
+            tweet_comments.content as comments, 
+            CASE
+              WHEN tweets.updated_at IS NOT NULL THEN
+                CASE
+                  WHEN TIMESTAMPDIFF(SECOND, tweets.updated_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(SECOND, tweets.updated_at, NOW()+1), ' seconds ago')
+                  WHEN TIMESTAMPDIFF(MINUTE, tweets.updated_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(MINUTE, tweets.updated_at, NOW()), ' minutes ago')
+                  WHEN TIMESTAMPDIFF(HOUR, tweets.updated_at, NOW()) < 24 THEN CONCAT(TIMESTAMPDIFF(HOUR, tweets.updated_at, NOW()), ' hours ago')
+                  ELSE CONCAT(DATE_FORMAT(tweets.updated_at, '%d'), ' ', DATE_FORMAT(tweets.updated_at, '%M'))
+                END
+              ELSE
+                CASE
+                  WHEN TIMESTAMPDIFF(SECOND, tweets.created_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(SECOND, tweets.created_at, NOW()+1), ' seconds ago')
+                  WHEN TIMESTAMPDIFF(MINUTE, tweets.created_at, NOW()) < 60 THEN CONCAT(TIMESTAMPDIFF(MINUTE, tweets.created_at, NOW()), ' minutes ago')
+                  WHEN TIMESTAMPDIFF(HOUR, tweets.created_at, NOW()) < 24 THEN CONCAT(TIMESTAMPDIFF(HOUR, tweets.created_at, NOW()), ' hours ago')
+                  ELSE CONCAT(DATE_FORMAT(tweets.created_at, '%d'), ' ', DATE_FORMAT(tweets.created_at, '%M'))
+                END
+            END as time,
+            medias.media_url as media_url
+            FROM users
+            JOIN tweets ON users.id = tweets.user_id
+            LEFT JOIN medias ON tweets.id = medias.tweet_id
+            LEFT JOIN tweet_comments ON tweet_comments.user_id = tweets.id 
+            WHERE  tweets.content LIKE '%demo%' OR users.username LIKE '%demo%' OR users.name LIKE '%demo%'
+                  ORDER BY time DESC
+          `
+            let [result] = await connection.query(sql);
+            return res.json({ resultTweet: result })
+
         } catch (error) {
             return res.json({ error: error })
         }
