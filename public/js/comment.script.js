3ca476172491693874aa379f77976c3fdeeb36cf
nikkipri@77 <nikita.kangad.2024@gmail.com>
2024-04-24 17:42:57 +0530

    comment replies

--- public/js/comment.script.js
+++ public/js/comment.script.js
@@ -18,7 +18,6 @@ function Comment(tweetId) {
                 location.reload();
             })
             .catch(error => console.error('Error:', error));
-
     }
     else {
         Swal.fire("Please add your comment!");
@@ -26,7 +25,7 @@ function Comment(tweetId) {
 }
 function Reply(comment_id) {
     console.log("OK");
-    let commentText = document.querySelector('#reply');
+    let commentText = document.querySelector(`.reply_${comment_id}`);
     if (commentText.value !== '') {
 
         fetch('/post_reply', {
@@ -36,18 +35,39 @@ function Reply(comment_id) {
             },
             body: JSON.stringify({
                 comment_id: comment_id,
-                comment: commentText.value,
+                content: commentText.value,
             }),
         })
             .then(response => response.json())
             .then(data => {
+                commentText.value = '';
+                location.reload();
             })
             .catch(error => console.error('Error:', error));
-
     }
-
 }
+async function showReply(comment_id) {
+    document.querySelector(`.showreply_${comment_id}`).style.display = 'flex';
 
+    let data = await fetch('/get_reply', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            comment_id: comment_id,
+        }),
+    });
+
+    data = await data.json();
+    console.log(data.replies);
+
+    let repliess = document.querySelector(`.replies_${comment_id}`);
+    data.replies.forEach(reply => {
+        let reply_list = `<ul><li class="border border-gray-50 bg-sky-200">${reply.content}</li></ul>`;
+        repliess.innerHTML += reply_list;
+    });
+}
 function show(tweetId) {
     document.querySelector(`.comments_${tweetId}`).style.display = 'block';
     document.querySelector(`.comments_${tweetId}`).style.opacity = '1';
--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const { getHome, get_comment, post_comment,get_notification,post_notification } = require('../controller/home.controller');
+const { getHome, get_comment, post_comment,get_notification,post_notification,post_reply,get_reply } = require('../controller/home.controller');
 
 
 
@@ -15,7 +15,8 @@ router.post("/post_notification", passport.authenticate('jwt', { session: false,
 // router.post("/home", getHome);
 router.get('/get_comments/:id', passport.authenticate('jwt', { session: false }), get_comment);
 router.post('/post_comments', passport.authenticate('jwt', { session: false }), post_comment);
-router.post('/post_reply', passport.authenticate('jwt', { session: false }), post_comment);
+router.post('/post_reply', passport.authenticate('jwt', { session: false }), post_reply);
+router.post('/get_reply', passport.authenticate('jwt', { session: false }), get_reply);
 
 
 module.exports = router;
