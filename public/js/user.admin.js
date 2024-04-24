cce36f21aa422b60724cff0ad42c490d49497259
mihir-rajpopat <mihir.rajpopat.2024@gmail.com>
2024-04-24 12:38:14 +0530

    make side bar of admin pannel and manages tab of the side bar

---
+++ public/js/user.admin.js
@@ -0,0 +1,17 @@
+
+async function getuserpage() {
+    // async function getdata() {
+    //     let url = window.location.origin + "/users"
+    //     let data = await fetch(url)
+    //     data = await data.json()
+    //     return data;
+    // }
+
+
+
+    // let data = await getdata();
+    let user = `<h2> hello </h1>`
+    return user;
+
+}
+
