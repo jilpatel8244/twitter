100b313d14d921890097038299544101e8d0499b
mihir-rajpopat <mihir.rajpopat.2024@gmail.com>
2024-04-27 17:44:03 +0530

    implement pagination and read csv data and insert users in database

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const logger = require("../../logger/logger");
-const { getAdminLogin, getUsers, getTweets, manageUserActivation, ristricTweet, getverifypage, getVerifiedRequest, updateverify, getAdminPannel, adminLoginHandler } = require("../controller/adminpannel/adminPannelControler");
+const { getAdminLogin, getUsers, getTweets, manageUserActivation, ristricTweet, getverifypage, getVerifiedRequest, updateverify, getAdminPannel, adminLoginHandler, addUserCsv, adduserbyform } = require("../controller/adminpannel/adminPannelControler");
 
 const passport = require("passport");
 const { permission } = require("../middleware/permission");
+const { uploadcsv } = require("../middleware/multer");
 require("../middleware/passport");
 
+
 const router = express.Router();
+router.post("/uploadcsv", uploadcsv.single("file"), addUserCsv)
 router.get("/adminlogin", getAdminLogin)
 router.post("/adminlogin", adminLoginHandler)
 router.post("/getusers", getUsers)
@@ -16,6 +19,9 @@ router.post("/gettweet", getTweets);
 router.post("/updateStatusUser", manageUserActivation);
 router.post("/ristrictweet", ristricTweet);
 router.post("/updateverify", updateverify);
+router.post("/adduser", adduserbyform);
+
+
 router.use(passport.authenticate('jwt', { session: false, failureRedirect: "/admin/adminlogin" }), permission)
 
 router.get("/adminPannel", getAdminPannel)
